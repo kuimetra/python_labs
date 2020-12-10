925925aa373912d1a359f9c674ed0ff085970cd1
Alina <alinaaartemiuk@gmail.com>
2020-12-10 15:47:29 +0200

    edit

--- module1/list_task2.py
+++ module1/list_task2.py
@@ -1,6 +1,5 @@
 # Ввести  масив  цілих  чисел.
 # Елементи  в  першій  половині  масиву  посортувати  за зростанням, а в другій –за спаданням.
-print('Enter elements of the list:', end=' ')
-l = [int(i) for i in input().split()]
+l = [int(i) for i in input('Enter elements of the list: ').split()]
 sorted_l = sorted(l[:len(l) // 2]) + sorted(l[len(l) // 2:], reverse=True)
 print(sorted_l)
